099d5076c08629e0194ab746c89d70f9d658ba54
graja21 <mohamedamine.graja@esprit.tn>
2025-11-25 19:27:35 +0100

    Add unit test and JaCoCo coverage

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,13 @@
 package com.example;
 
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello from Maven project!");
+        System.out.println(getMessage());
+    }
+
+    public static String getMessage() {
+        return "Hello from Maven project!";
     }
 }
+
---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    void messageIsCorrect() {
+        String msg = App.getMessage();
+        assertEquals("Hello from Maven project!", msg);
+    }
+}

ff45badcd38ceadf48424834799f7e3671f148e8
graja21 <mohamedamine.graja@esprit.tn>
2025-11-29 16:12:57 +0100

    Fix SonarQube code smells: replace println and add private constructor

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,16 @@
 package com.example;
 
+import java.util.logging.Logger;
+
 public class App {
+
+    // Logger pour remplacer System.out.println
+    private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
+    // Constructeur privé pour empêcher l'instanciation (classe utilitaire)
+    private App() {
+        // rien
+    }
 
     // Méthode utilisée par le test
     public static String getGreeting() {
@@ -8,7 +18,8 @@ public class App {
     }
 
     public static void main(String[] args) {
-        // On affiche le message
-        System.out.println(getGreeting());
+        // On affiche le message via le logger
+        LOGGER.info(getGreeting());
     }
 }
+
